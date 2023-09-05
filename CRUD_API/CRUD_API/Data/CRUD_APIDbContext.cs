8e1467795ffd1360888e9822503a8f56c60408e4
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-09-05 16:03:27 +0530

    First Commit

---
+++ CRUD_API/CRUD_API/Data/CRUD_APIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CRUD_API.Models;
+
+namespace CRUD_API.Data
+{
+    public class CRUD_APIDbContext : DbContext
+    {
+        public CRUD_APIDbContext (DbContextOptions<CRUD_APIDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CRUD_API.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ CRUD_API/CRUD_API/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using CRUD_API.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<CRUD_APIDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CRUD_APIDbContext") ?? throw new InvalidOperationException("Connection string 'CRUD_APIDbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
